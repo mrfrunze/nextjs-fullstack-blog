32721843ff3133fecfbca44fba0e5f94c3775f22
Ilya <stoynov.oleg@gmail.com>
2024-10-24 15:14:37 +0200

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,16 +4,17 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.pexels.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
+        hostname: "plus.unsplash.com",
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos', // Добавляем picsum.photos как разрешённый хост
         pathname: '/**', // Разрешаем любые пути на этом хосте
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
       },
     ],
   },
